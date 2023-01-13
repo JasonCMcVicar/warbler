877eca2ee9c4e47711df2a680f058487e48e18d2
Jason <jasoncmcvicar@gmail.com>
2023-01-13 10:24:08 -0600

    cleaning up code

--- app.py
+++ app.py
@@ -17,8 +17,6 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-# link = os.getenv('DATABASE_URL','fail')
-# print (link)
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
