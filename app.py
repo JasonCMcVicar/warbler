e9f4edecf9a926a8fa945269a1ab8de540770df2
Jason <jasoncmcvicar@gmail.com>
2023-01-12 22:13:38 -0600

    working through seeing if I can keep replace method in with variable strategy

--- app.py
+++ app.py
@@ -18,27 +18,17 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+link = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = link
 # os.environ['DATABASE_URL'].replace("postgres://", "postgresql://")
 # app.config['SQLALCHEMY_DATABASE_URI'] = (
 #     os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+# app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 toolbar = DebugToolbarExtension(app)
-
-# # Check if the database needs to be initialized
-# engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-# inspector = sa.inspect(engine)
-# if not inspector.has_table("users"):
-#     with app.app_context():
-#         db.drop_all()
-#         db.create_all()
-#         app.logger.info('Initialized the database!')
-# else:
-#     app.logger.info('Database already contains the users table.')
 
 connect_db(app)
 
