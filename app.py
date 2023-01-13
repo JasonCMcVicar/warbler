56d16db9284db62b021775878a00a70f0957806b
Jason <jasoncmcvicar@gmail.com>
2023-01-12 21:11:45 -0600

    put replace method back in for postgres and postgresql

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.getenv('DATABASE_URL').replace("postgres://", "postgresql://"))
 # app.config['SQLALCHEMY_DATABASE_URI'] = (
 #     os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
