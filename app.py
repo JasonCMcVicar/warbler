5b0c89cdc128b248cef25b3185fe060d00dbdbd0
Jason <jasoncmcvicar@gmail.com>
2023-01-12 21:32:28 -0600

    reworking how to incorporate replace method

--- app.py
+++ app.py
@@ -18,8 +18,9 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1))
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 # app.config['SQLALCHEMY_DATABASE_URI'] = (
 #     os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
